9e0a70b32a16663a4d4dc8ebaee0d89cd3d88d86
radadiyasmit5 <radadiyasmit5@gmail.com>
2022-03-25 18:56:11 -0400

    auth controller updated

--- src/main/java/com/example/demo/controller/Authcontroller.java
+++ src/main/java/com/example/demo/controller/Authcontroller.java
@@ -1,10 +1,7 @@
 package com.example.demo.controller;
 
-import com.example.demo.Exception.UsernotfoundException;
-import com.example.demo.Security.CustomUserDetailsService;
-import com.example.demo.payload.OtvcValidationRequest;
-import com.example.demo.utils.Jwtutilsproviders;
 import com.example.demo.payload.LoginDto;
+import com.example.demo.payload.OtvcValidationRequest;
 import com.example.demo.payload.SignupDto;
 import com.example.demo.payload.SignupResponseDTO;
 import com.example.demo.service.impl.impl.AuthenticationAndRegisterServiceimpl;
@@ -12,11 +9,6 @@ import com.example.demo.utils.Otvcutils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
@@ -27,14 +19,7 @@ import java.io.UnsupportedEncodingException;
 @CrossOrigin
 public class Authcontroller {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
 
-    @Autowired
-    private CustomUserDetailsService customeuserDetailsService;
-
-    @Autowired
-    private Jwtutilsproviders jwtutilsproviders;
 
     @Autowired
     private Otvcutils otvcutils;
@@ -81,26 +66,9 @@ public class Authcontroller {
     }
 
     @PostMapping("/login")
-    public String userLogin(@RequestBody LoginDto logindto) {
+    public ResponseEntity<String> userLogin(@RequestBody LoginDto logindto) {
 
-        try {
-            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(logindto.getUsername(), logindto.getPassword()));
-
-        } catch (BadCredentialsException e) {
-            throw new UsernotfoundException("Sorry, your password was incorrect. Please double-check your password. ");
-        } catch (Exception e) {
-//            throw new Exception(e.getMessage());
-            if (e.getMessage() == "user not found") {
-                throw new UsernotfoundException("The username you entered doesn't belong to an account. Please check your username and try again.");
-
-            } else {
-                throw new UsernotfoundException("there is some problem while authenticating user info please try to enter your username and password again");
-            }
-        }
-
-        UserDetails userdetails = customeuserDetailsService.loadUserByUsername(logindto.getUsername());
-        String token = jwtutilsproviders.generateToken(userdetails);
-        return token;
+      return  new ResponseEntity(signupimplservice.userLogin(logindto),HttpStatus.OK);
     }
 
 
